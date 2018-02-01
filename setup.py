10aaea7b44f49de97d28946b645f39258f0b8208
lazylife7157 <lazylife7157@gmail.com>
2018-02-02 01:27:22 +0900

    Add sub-modules, license, install requires to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,12 @@ setup(
   name='coinone.py',
   version='0.1.0',
   description='Simple coinone api wrapper',
-  packages=['coinone']
+  license='Apache License 2.0',
+  packages=[
+    'coinone',
+    'coinone.api',
+    'coinone.api.v1',
+    'coinone.api.v2',
+  ],
+  install_requires=['requests']
 )
